56732de91b70c8d792b26156f07bb8adf81ab98b
Tushar Ojha <tusharojha2001@gmail.com>
2020-06-20 16:53:21 +0530

    comments and readme added

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# flutterauthentication
+# Flutter Authentication App
 
-A new Flutter application.
+A new Flutter Authentication App UI using Firebase Authentication with Sign In options like email, phone, social login, etc.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+- Fork this project to your repository.
+- Clone your forked repository to local machine
+- Now work the repository and make wonderful apps
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Follow the comments provided in `main.dart` file for full explanation of UI design
--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,63 @@
-import 'package:flutter/cupertino.dart';
+// Importing Packages that contains all widgets we use
 import 'package:flutter/material.dart';
 
+// Program Starts from here the Main Function
 void main() {
   runApp(MyApp());
 }
 
+// MyApp is the main Class in which the whole app runs & it is a stateless widget
+// because it does not maintain state in it directly.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowCheckedModeBanner is set to false so that the debug badge from UI is removed
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomeScreen(), // HomeScreen is the landing page of the app
     );
   }
 }
 
+// HomeScreen Widget is set to be Stateful but it is also not handling state
+// as of now since, its just UI but while implementing features you need to add state variables
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // Build method runs again and again as the state changes to rebuild the UI
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // SingleChildScrollView makes the UI not to overflow from bottom when TextField is used or different device is used
       body: SingleChildScrollView(
         child: Container(
+          // MediaQuery.of(context).size provides the Dimensions of the parent widget
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
+            // Adding Linear Gradient to the background of UI
             gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
+              // Colors are converted to Integer from Hex Codes by replacing # with 0xff
               colors: [Color(0xffFBB034), Color(0xffF8B313)],
             ),
           ),
-          child: Column(
+          child: Column( // Column Widget is added to Render the complete UI in vertical direction
             children: <Widget>[
+              // App Bar is added in the body parameter of the Scaffold because we need to make it transparent and
+              // show the gradient in background. Alternative option will be to use gradient action bar from pub.dev
               AppBar(
                 backgroundColor: Colors.transparent,
                 elevation: 0,
-                title: Text("LOGIN", style: TextStyle(fontWeight: FontWeight.bold),),
+                // elevation removes the shadow under the action bar
+                title: Text(
+                  "LOGIN", style: TextStyle(fontWeight: FontWeight.bold),),
                 centerTitle: true,
                 leading: Icon(Icons.arrow_back),
+                // Actions are identified as buttons which are added at the right of App Bar
                 actions: <Widget>[
                   Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -49,10 +65,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   )
                 ],
               ),
-              ClipPath(
+              ClipPath( // ClipPath is used to clip the child in a custom shape
                 clipper: BottomClipper(),
+                // here is the custom clipper for bottom cut shape
                 child: Container(
-                  width: MediaQuery.of(context).size.width,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
                   padding: EdgeInsets.only(top: 40, bottom: 30),
                   margin: EdgeInsets.only(top: 30, left: 20, right: 20),
                   decoration: BoxDecoration(
@@ -91,7 +111,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         alignment: Alignment.centerRight,
                         child: Padding(
                           padding: const EdgeInsets.only(right: 18.0),
-                          child: InkWell(
+                          child: InkWell( // InkWell widget makes the widget clickable and provide call back for touch events
                             onTap: () {
                               print("Forget Password tap");
                             },
@@ -113,17 +133,17 @@ class _HomeScreenState extends State<HomeScreen> {
                           onTap: () {
                             print("login using mobile number tap");
                           },
-                          child: RichText(
+                          child: RichText( // RichText is used to styling a particular text span in a text by grouping them in one widget
                             text: TextSpan(
                               style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,),
                               text: 'Login using ',
                               children: <TextSpan>[
                                 TextSpan(
                                   text: 'Mobile Number and OTP',
                                   style: TextStyle(
-                                      decoration: TextDecoration.underline),
+                                    decoration: TextDecoration.underline,),
                                 )
                               ],
                             ),
@@ -155,7 +175,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               ClipPath(
-                clipper: TopClipper(),
+                clipper: TopClipper(), // Custom Clipper for top clipping the social login menu box
                 child: Container(
                   width: MediaQuery.of(context).size.width,
                   padding: EdgeInsets.only(top: 50, bottom: 50),
@@ -167,7 +187,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           color: Colors.black,
                           offset: Offset(1, 2),
                           spreadRadius: 1.0,
-                          blurRadius: 5.0)
+                          blurRadius: 5.0),
                     ],
                     borderRadius: BorderRadius.circular(20),
                   ),
@@ -193,7 +213,8 @@ class _HomeScreenState extends State<HomeScreen> {
                         children: <Widget>[
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Image.asset('assets/fb_icon.png'),
+                            child: Image.asset('assets/fb_icon.png'), // loading custom images from assets in Flutter
+                            // NOTE that if you have not addressed these images in pubspec.yaml then it will show error
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
@@ -235,17 +256,19 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+// Custom Clipper Class
 class BottomClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-
+    // Add Path lines to form slight cut
     path.lineTo(size.width, 0);
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height - 50);
     return path;
   }
 
+  // we don't need to render it again and again as UI renders
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
